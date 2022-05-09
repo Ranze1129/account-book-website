a061867c9f10dd4faa2a0eb78f9eaf2740329dc2
Ranze <1066467243@qq.com>
2022-05-09 17:53:38 +0800

    deploy

---
+++ precache-manifest.6808e7f007e5e7ca7dd8c64fecb0cc8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39994f0b6845ebeff85e",
+    "url": "/account-book-website/css/app.e315bca9.css"
+  },
+  {
+    "revision": "91bdd6491315babdb861147fb1ed97ea",
+    "url": "/account-book-website/index.html"
+  },
+  {
+    "revision": "39994f0b6845ebeff85e",
+    "url": "/account-book-website/js/app.59298264.js"
+  },
+  {
+    "revision": "907c2f668c875a0c7b3f",
+    "url": "/account-book-website/js/chunk-vendors.89e17db7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/account-book-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-book-website/robots.txt"
+  }
+]);
